d5e4c585a3706b679fa77e4520e64fa6dd041f34
Karl <toom.karl42@gmail.com>
2023-03-21 15:40:09 +0200

    completed ecercise 2

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-/* Exercise 1:
+/*Exercise 1:
+Write a JavaScript program to check two numbers and return true if one of the number is
+100 or if the sum of the two numbers is 100
 
-Write a Javascript program to check two 
-numbers and return true if one of the numbers
- is 100 or if the sum of the two numbers is
-  100
+Exercise 2:
+Write a JavaScript exercise to get the extension of a filename.
 
+Exercise 3:
+Write a JavaScript program to replace every character in a given string with the character
+following it in the alphabet
+
+Exercise 4:
+Write a JavaScript program to get the current date.
+Expected Output : mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy
+
+Exercise 5:
+Write a JavaScript program to create a new string adding "New!" in front of a given string. If
+the given string begins with "New!" already then return the original string.
 */
 
-const isEqualTo100 = (a,b) => a ===100 || b === 100 || (a+b) === 100;
-
-console.log(isEqualTo100(100,0));
-console.log(isEqualTo100(0,50));
-console.log(isEqualTo100(50,50));
+const getFileExtension = (str) => str.slice(str.lastIndexOf('.'));
+console.log(getFileExtension('index.html'));
