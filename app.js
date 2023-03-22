3cd26cb2d6d1b9f76082b3789d539420fbbe9607
Karl <toom.karl42@gmail.com>
2023-03-22 16:06:02 +0200

    using variables to change text inside of the html

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
-/*Exercise 1:
-Write a JavaScript program to check two numbers and return true if one of the number is
-100 or if the sum of the two numbers is 100
 
-Exercise 2:
-Write a JavaScript exercise to get the extension of a filename.
+let firstName = "Karl"
+let age = 21;
+let student = true;
 
-Exercise 3:
-Write a JavaScript program to replace every character in a given string with the character
-following it in the alphabet
+console.log(firstName)
+console.log(age)
+console.log(student)
 
-Exercise 4:
-Write a JavaScript program to get the current date.
-Expected Output : mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy
-
-Exercise 5:
-Write a JavaScript program to create a new string adding "New!" in front of a given string. If
-the given string begins with "New!" already then return the original string.
-*/
-
-const getFileExtension = (str) => str.slice(str.lastIndexOf('.'));
-console.log(getFileExtension('index.html'));
+document.getElementById("p1").innerHTML = "hello " + firstName;
+document.getElementById("p2").innerHTML = "You are " + age + " years old";
+document.getElementById("p3").innerHTML = "Enrolled " + student;
