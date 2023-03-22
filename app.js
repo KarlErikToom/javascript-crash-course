3518bc86ca43f25bd7cb13368dc46b59f30deb12
Karl <toom.karl42@gmail.com>
2023-03-22 16:22:01 +0200

    made a label to get a users name

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
+let username;
 
-let firstName = "Karl"
-let age = 21;
-let student = true;
-
-console.log(firstName)
-console.log(age)
-console.log(student)
-
-document.getElementById("p1").innerHTML = "hello " + firstName;
-document.getElementById("p2").innerHTML = "You are " + age + " years old";
-document.getElementById("p3").innerHTML = "Enrolled " + student;
+document.getElementById("myButton").onclick = function(){
+  username = document.getElementById("myText").value ;
+  console.log(username) 
+  document.getElementById("myLabel").innerHTML = "Hello " + username;
+}
