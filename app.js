9e0851b264e3506450375a33e65408108a485f21
Karl <toom.karl42@gmail.com>
2023-03-23 15:18:18 +0200

    AND OR operators

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
 
-let grade = 50;
+let temp =15;
+let sunny = true;
 
-switch(true){
-    case grade >=90:
-        console.log("you did great")
-        break;
-        case grade >=80:
-            console.log("you did good");
-            break
-            case grade >=70:
-            console.log("you did okay");
-            break;
-            case grade >=60:
-                console.log("you barely passed");
-                break;
-                case grade <60:
-                    console.log("you FAILED");
-                    break;
-                    default:
-                        console.log(grade, "is not a passing grade")
+if(temp > 0 && temp <30 && sunny){
+    console.log("the weather is good")
+}
+else{
+    console.log("the weather is bad")
 }
