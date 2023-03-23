56ce759a981a3ed3f473d7beacab91b5a01d97df
Karl <toom.karl42@gmail.com>
2023-03-23 14:42:17 +0200

    if statements

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-let userName = "karl";
+/*let age = 67;
 
-let letter = userName.charAt(0).toUpperCase();
+if(age >=65){
+    console.log("u are old AF")
+}
+else if(age >=18){
+console.log("u are and adult")
+}
+else if(age < 0){
+    console.log("you arent born")
+}
+else{
+    console.log("u a child")
+}
+*/
 
+let online = false;
 
-console.log(letter)
+if(online){
+    console.log("you are online")
+}
+else{
+    console.log("you offline")
+}
