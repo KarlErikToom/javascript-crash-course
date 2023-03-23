e250deb5e4fd79af987ff2c1606060449dfe72cd
Karl <toom.karl42@gmail.com>
2023-03-23 14:35:44 +0200

    method chaining

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
-let count = 0;
+let userName = "karl";
 
-document.getElementById("decreaseBtn").onclick = function (){
-    count -= 1;
-    document.getElementById("countLabel").innerHTML = count
-}
-document.getElementById("resetBtn").onclick = function (){
-    count = 0;
-    document.getElementById("countLabel").innerHTML = count
-}
-document.getElementById("increaseBtn").onclick = function (){
-    count += 1;
-    document.getElementById("countLabel").innerHTML = count
-}
+let letter = userName.charAt(0).toUpperCase();
+
+
+console.log(letter)
