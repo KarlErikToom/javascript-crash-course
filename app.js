4b722209109de3d3682b6d706c83ac4076b387fc
Karl <toom.karl42@gmail.com>
2023-03-24 18:15:57 +0200

    template literals

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+let userName ="toobal";
+let items = 3;
+let total = 65;
 
-checkWinner(false)
+//console.log("hello", userName);
+//console.log("you have", items, "items in ur cart");
+//console.log("your total is", total)
 
-function checkWinner(win){
-    win ? console.log("you win") : console.log("you lose")
-}
+//console.log(`hello ${userName}`);
+//console.log(`you have ${items} items in your cart`);
+//console.log(`your total is $${total}`)
+
+let text =
+`hello ${userName} <br>
+you have ${items} items in your cart<br>
+your total is $${total} <br>`;
+
+//console.log(text)
+document.getElementById("myLabel").innerHTML = text;
