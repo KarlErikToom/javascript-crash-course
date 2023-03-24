20296273db7a0a8c886f0a097d905655412523fd
Karl <toom.karl42@gmail.com>
2023-03-24 17:38:22 +0200

    nested loops

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 
-for(let counter = 1; counter <=10; counter +=1){
-    console.log(counter)
+let symbol = window.prompt("enter symbol")
+let rows = window.prompt("enter # of rows");
+let columns = window.prompt("enter # of columns")
+
+for (let i=1; i <=rows; i+=1){
+    for (let j=1; j <=columns; j+=1){
+    document.getElementById("myRectangle").innerHTML += symbol;
+ }
+ document.getElementById("myRectangle").innerHTML += "<br>";
 }
