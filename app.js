cddb3f594ebfeac2c8b154be3054825468db22ba
Karl <toom.karl42@gmail.com>
2023-03-28 19:09:33 +0300

    sort arrays in alpabetical order

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-let prices = [5, 10, 15, 20];
 
-/*for(let i = prices.length - 1; i >= 0; i -=1){
-console.log(prices[i]);
-}
-*/
 
-for(let price of prices){
-  console.log(price);
+let fruits = ["banana", "apple", "orange", "mango"];
+
+//fruits = fruits.sort();
+fruits = fruits.sort().reverse();
+for(let fruit of fruits){
+  console.log(fruit);
 }
