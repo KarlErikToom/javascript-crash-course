ad19c5f110dd56f131650783bd81d7d023e0ea7b
Karl <toom.karl42@gmail.com>
2023-03-28 18:52:09 +0300

    made temp conversion program

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
-const answer = Math.floor(Math.random() * 10 +1);
-let guesses = 0;
+document.getElementById("submitButton").onclick = function(){
+  let temp;
 
-document.getElementById("submitButton").onclick =function(){
-  let guess =  document.getElementById("guessField").value
-  guesses+=1;
+  if(document.getElementById("cButton").checked){
+    temp = document.getElementById("textBox").value;
+    temp = Number(temp);
+    temp = toCelsius(temp);
+    document.getElementById("tempLabel").innerHTML = temp + "°C";
 
-  if (guess == answer){
-    alert(`${answer} is the #. It took you ${guesses} guesses`)
   }
-  else if (guess < answer){
-    alert("Too small");
+
+  else if (document.getElementById("fButton").checked){
+    temp = document.getElementById("textBox").value;
+    temp = Number(temp);
+    temp = toFahrenheit(temp);
+    document.getElementById("tempLabel").innerHTML = temp + "°F";
+
   }
+
   else{
-    alert("too large")
+    
+    document.getElementById("tempLabel").innerHTML = "select a unit"
   }
+}
+
+function toCelsius(temp){
+return (temp - 32)* (5/9);
+}
+
+
+
+function toFahrenheit(temp){
+  return temp * 9 / 5 +32;
 }
