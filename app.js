ef2072fd1d78c8c5eddf4c30e37ee78e4f57109a
Karl <toom.karl42@gmail.com>
2023-03-28 19:28:13 +0300

    rest parameters

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 
-/*//let userName = "karl toobal";
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-//console.log(...userName)
+let a = 1
+let b = 2
+let c = 3
+let d = 4
+let e = 5
 
-let maximum = Math.max(...numbers);
-console.log(maximum)
+console.log(sum(c, d));
 
-*/
-
-let class1 = ["karl", "kaermo", "jaakop"]
-let class2 = ["jon", "max", "jason"];
-
-class1.push(...class2);
-
-console.log(...class1)
+function sum(...numbers){
+let total = 0;
+for(let number of numbers){
+  total += number
+}
+return total
+}
