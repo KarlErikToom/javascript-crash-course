9e8c856da4c1e77c3555e3b939078c7949f12469
Karl <toom.karl42@gmail.com>
2023-04-18 16:09:09 +0300

    for loop training

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
-let cash = 40
-let price = 40
-let isStoreOpen = true
+/**Write a for-loop that loops through 1 to 20
+ * if the number is divisible by 3, print "frontend"
+ * if the number is divisible by 5, print "simplified"
+ * if the number is divisible by 3 and 5, print "frontend simplified"
+ * if the number is *not* divisible by either 3 or 5, print the number
+ * 
+ * @example
+ * 1 -> 1
+ * 2 -> 2
+ * 3 -> "frontend"
+ * 4 -> 4
+ * 5 -> "simplified"
+ * ...
+ * 15 -> "frontend simplified"
+ * ...
+ * 20 ->simplified
+ */
 
-
-let str = cash >= price && isStoreOpen ? `give receipt` : `do not give receipt`
-console.log(str)
+for ( let i =1; i<=20; i++){
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log(`${i} -> Frontend Simplified`)
+    }
+    else if ( i % 3 === 0){
+        console.log(`${i} -> Frontend`)
+    }
+    else if ( i % 5 ===0){
+        console.log(`${i}-> Simplified`)
+    }
+    else{
+        console.log(`${i} -> ${i}`)
+    }
+}
