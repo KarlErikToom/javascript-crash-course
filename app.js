66717d90791faa32142a65925dd718cb91ecce9b
Karl <toom.karl42@gmail.com>
2023-04-18 16:42:22 +0300

    function exercise

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-/**Print out every character from the string:
- * "Frontend Simplified"
+/**Create a function that converts Celsius to Fahrenheit
+ * 
+ * Celsius to Fahrenheit formula:
+ * F = C x 1.8 + 32
+ * 
  * 
  * @example
- * `F`
- * `r`
- * `o`
- * `n`
- * ...
- * `e`
- * `d`
+ * 
+ * convertCelsiusToFahrenheit(0) ->32
+ * convertCelsiusToFahrenheit(10) ->50
+ * convertCelsiusToFahrenheit(30) ->86
  */
 
-let str = "Frontend Simplified"
-
-
-for (let i = 0; i < str.length; i++){
-    console.log(str[i])
+const convertCelsiusToFahrenheit = (celsius) => {
+ return celsius * 1.8 + 32
 }
+
+console.log(convertCelsiusToFahrenheit(0))
+console.log(convertCelsiusToFahrenheit(10))
+console.log(convertCelsiusToFahrenheit(30))
