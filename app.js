80e23769e02ae000bc2a8140ebc42421b36b9e13
Karl <toom.karl42@gmail.com>
2023-04-18 17:20:15 +0300

    array filter exercise

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-/**Create a function that converts Celsius to Fahrenheit
- * 
- * Celsius to Fahrenheit formula:
- * F = C x 1.8 + 32
- * 
- * 
+/*let arr = [20, 30, 40, 50, 100,];
+
+let newArr = arr.filter(element => element < 50
+);
+
+console.log(newArr);
+*/
+
+/**
+ * Filter out all the `FAIL` elements in an array
+ *
  * @example
- * 
- * convertCelsiusToFahrenheit(0) ->32
- * convertCelsiusToFahrenheit(10) ->50
- * convertCelsiusToFahrenheit(30) ->86
+ * [`A+`, `A`, `FAIL`] => [`A+`, `A`]
+ * [`FAIL`, `FAIL`, `B`] => [`B`]
+ * [`FAIL`] => []
  */
 
-const convertCelsiusToFahrenheit = (celsius) => {
- return celsius * 1.8 + 32
-}
+let grades = [`A+`, `A`, `FAIL`];
 
-console.log(convertCelsiusToFahrenheit(0))
-console.log(convertCelsiusToFahrenheit(10))
-console.log(convertCelsiusToFahrenheit(30))
+let goodGrades = grades.filter(element =>element !== `FAIL`);
+;
+console.log(goodGrades);
