607a2d7d6e45086828288e4c02aeba1c9af9ea5f
Karl <toom.karl42@gmail.com>
2023-04-18 18:24:35 +0300

    objects

--- app.js
+++ app.js
@@ -1,15 +1,49 @@
-/*let arr = [1, 4, 9, 16];
+let users = [
+  {
+    username: "karl",
+    email: "toom.karl42@gmail.com",
+    password: `test123`,
+    subscriptionStatus: "VIP",
+    discordId: "toobal#1111",
+    lessonsCompleted: [0, 1],
+  },
+  {
+    username: "toom",
+    email: "mister42@gmail.com",
+    password: `bro123`,
+    subscriptionStatus: "VIP",
+    discordId: "mooon#1111",
+    lessonsCompleted: [0, 1, 2, 3],
+  },
+];
 
-let newArr = arr.map(element => undefined);
-;
-console.log(newArr);
-*/
-
-let dollars = [1, 5, 10, 3];
-
-let cents = []
-
-for (let i =0; i < dollars.length; i++){
-    cents.push(dollars[i] * 100)
+function login(email, password) {
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].email === email) {
+      console.log(users[i]);
+      if (users[i].password === password) {
+        console.log(`log the user in - details are correct`);
+      } else {
+        console.log(`password is incorrect -try again`);
+      }
+      return;
+    }
+  }
+  console.log(`could not find an email that matches`);
 }
-console.log(cents)
+
+login(`toom.karl42@gmail.com`, `test123`);
+
+function register(user) {
+  users.push(user);
+}
+
+register({
+  username: "zen",
+  email: `zen@mail.ee`,
+  password: `bigbog`,
+  subscriptionStatus: `VIP`,
+  discordId: `pog#1110`,
+  lessonsCompleted: [0, 1],
+});
+console.log(users);
