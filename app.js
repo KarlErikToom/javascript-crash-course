6137af2dfcb7fa7ff00ad1ac625f71091938831a
Karl <toom.karl42@gmail.com>
2023-04-18 16:18:15 +0300

    another for loop exercise

--- app.js
+++ app.js
@@ -1,32 +1,19 @@
-/**Write a for-loop that loops through 1 to 20
- * if the number is divisible by 3, print "frontend"
- * if the number is divisible by 5, print "simplified"
- * if the number is divisible by 3 and 5, print "frontend simplified"
- * if the number is *not* divisible by either 3 or 5, print the number
+/**Print out every character from the string:
+ * "Frontend Simplified"
  * 
  * @example
- * 1 -> 1
- * 2 -> 2
- * 3 -> "frontend"
- * 4 -> 4
- * 5 -> "simplified"
+ * `F`
+ * `r`
+ * `o`
+ * `n`
  * ...
- * 15 -> "frontend simplified"
- * ...
- * 20 ->simplified
+ * `e`
+ * `d`
  */
 
-for ( let i =1; i<=20; i++){
-    if (i % 3 === 0 && i % 5 === 0){
-        console.log(`${i} -> Frontend Simplified`)
-    }
-    else if ( i % 3 === 0){
-        console.log(`${i} -> Frontend`)
-    }
-    else if ( i % 5 ===0){
-        console.log(`${i}-> Simplified`)
-    }
-    else{
-        console.log(`${i} -> ${i}`)
-    }
+let str = "Frontend Simplified"
+
+
+for (let i = 0; i < str.length; i++){
+    console.log(str[i])
 }
